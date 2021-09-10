86b8665122f662cdd25d80f0328290e45b88106f
DragonPower84 <patrobighnesh8@gmail.com>
2021-09-10 13:44:08 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -22,8 +22,7 @@ try:
     DEV = 1477711713
     OWNER = config("OWNER", default="1477711713")
     FFMPEG = config(
-        "FFMPEG",
-        default='ffmpeg -i '''{}'''  -filter_complex "drawtext=fontfile=njnaruto.ttf:fontsize=35:fontcolor=white:bordercolor=black@0.50:x=30:y=30:text='Animes-Encoded':enable='between(t,0,10)':alpha='if(lt(t,9)\,1\,if(lt(t\,10)\,(1-(t-9))/1\,0))', drawtext=fontfile=njnaruto.ttf:text='\|\| Download From The Original Place https\://t.me/Animes_Encoded \|\| And For Animes Request https\://t.me/Anime_Hub_Group':bordercolor=black@0.50:borderw=5:fontcolor=white:fontsize=33:x=w-((2*w-200)*(t-615)/60):y=lh+0.5:enable='between(t, 615,680)':alpha='if(lt(t,679)\,1\,if(lt(t\,680)\,(1-(t-679))/1\,0))'[out1]" -metadata:s:a:0 title="[Telegram: @Animes_Encoded]" -metadata:s:a:1 title="[Telegram: @Animes_Encoded]" -metadata:s:s:0 title="[Telegram: @Animes_Encoded]~English" -metadata:s:s:1 title="[Telegram: @Animes_Encoded]~English" -map [out1] -map 0:a? -map 0:s? -map 0:t? -metadata title="Animes_Encoded - 480p" -c:v libx265 -pix_fmt yuv420p -preset medium -s 846x480 -crf 31.8 -c:a libfdk_aac -profile:a aac_he_v2  -ac 2 -vbr 2 -c:s copy  -movflags +faststart '''{}'''  -y',
+        "FFMPEG", default='ffmpeg -i "{}" -preset ultrafast -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{}"',
     )
     THUMB = config(
         "THUMBNAIL", default="https://telegra.ph/file/6d122eb240c33b6c10180.jpg"
